95603908dc2ba9954d99aa088f5e0169da1fcf38
jzhao23 <jasonjinzhao99@gmail.com>
2015-10-25 00:36:14 -0400

    more stuff

--- app.js
+++ app.js
@@ -1,64 +1,80 @@
-
-
-var app = angular.module('ZilkrForums', ['ui.router']);
-
-/*angular.module('ZilkrForums', ['ui.router']);*/
-
-/*var app = angular.module('ZilkrForums');*/
-
-app.config([
+angular.module('ZilkrForums', ['ui.router'])
+.config([
 '$stateProvider',
 '$urlRouterProvider',
 function($stateProvider, $urlRouterProvider) {
 
-  $stateProvider
-    .state('home', {
-      url: '/home',
-      templateUrl: '/home.html',
-      controller: 'MainCtrl'
-    });
+    $stateProvider
+        .state('home', {
+            url: '/home',
+            templateUrl: '/home.html',
+            controller: 'MainCtrl'
+        })
 
-  $urlRouterProvider.otherwise('home');
-}]);
+        .state('posts', {
+            url: '/posts/{id}',
+            templateUrl: '/posts.html',
+            controller: 'PostsCtrl'
+        })
 
-app.factory('posts', [function(){
-    
-    var o = {
-        
-        posts: []
-    };
-    
-    return o;
-}]);
+    $urlRouterProvider.otherwise('home');
+}])
 
-app.controller('MainCtrl', [ '$scope', 'posts',function($scope, posts){
-
-    
+.controller('MainCtrl', [
+'$scope',
+'posts',
+function($scope, posts){
     $scope.test = 'Hello world!';
     $scope.posts = posts.posts;
+
+    $scope.addPost = function() {
+        if ($scope.title === '') {
+            return;
+        }
+        $scope.posts.push({
+            title: $scope.title,
+            link: $scope.link,
+            upvotes: 0,
+            comments: [
+              /*{author: 'Joe', body: 'Cool post!', upvotes: 0},
+              {author: 'Bob', body: 'Great idea but everything is wrong!', upvotes: 0}*/
+            ]
+        });
+        $scope.title = '';
+        $scope.link = '';
+    };
     
-    /*[
-  {title: 'post 1', upvotes: 5},
-  {title: 'post 2', upvotes: 2},
-  {title: 'post 3', upvotes: 15},
-  {title: 'post 4', upvotes: 9},
-  {title: 'post 5', upvotes: 4}
-];*/
-    
-    $scope.addPost = function(){
-    if(!$scope.title || $scope.title === '') { return; }
-  $scope.posts.push({title: $scope.title, link: $scope.link, upvotes: 0});
-  
-  $scope.title = '';
-  $scope.link = '';
-};
     $scope.incrementUpvotes = function(post) {
         post.upvotes += 1;
-};
-    
-    
-    
-    
-    
-    }]);
+    }
+
+}])
+
+.controller('PostsCtrl', [
+'$scope',
+'$stateParams',
+'posts',
+function($scope, $stateParams, posts) {
+    $scope.post = posts.posts[$stateParams.id];
+
+    $scope.addComment = function(){
+        if($scope.body === '') { return; }
+        $scope.post.comments.push({
+            body: $scope.body,
+            author: 'user',
+            upvotes: 0
+        });
+        $scope.body = '';
+    };
+    $scope.incrementUpvotes = function(comment){
+        comment.upvotes += 1;
+    };
+}])
+
+.factory('posts', [function(){
+    var o = {
+        posts: []
+    };
+    return o;
+}])
 
